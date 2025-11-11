c4b3495d64861bb267533db677c1f9a359a1548a
TK-Pklgs <talhakhalilsqa@paklogics.com>
2025-11-12 00:07:44 +0500

    Minor updates

--- Chapter 1 (Getting Started)/Practice_Set (Chapter 1).py
+++ Chapter 1 (Getting Started)/Practice_Set (Chapter 1).py
@@ -1,7 +1,7 @@
 """Question No 1"""
 # Write a program to print Twinkle twinkle little star poem in python.
 
-# print("Twinkle Twinkle little start how I wonder what you are")
+print("Twinkle Twinkle little start how I wonder what you are")
 
 """Question No 2"""
 # Use REPL and print the table of 5 using it. 
@@ -10,14 +10,14 @@
 """Question # 3"""
 # Install an external module and use it to perform an operation of your interest. 
 
-# import emoji
-# print(emoji.emojize("Python is Fun :snake:"))
-# name = "Talha"
-# print(emoji.emojize(f"Hello {name}! :wave:", language='alias'))
+import emoji
+print(emoji.emojize("Python is Fun :snake:"))
+name = "Talha"
+print(emoji.emojize(f"Hello {name}! :wave:", language='alias'))
 
 """Question # 4"""
 # Write a python program to print the contents of a directory using the os module.
 # Search online for the function which does that. 
 
-# import os
-# print(os.listdir("C:\\Users\Hp\Desktop\Test Data"))
+import os
+print(os.listdir("C:\\Users\Hp\Desktop\Test Data"))
