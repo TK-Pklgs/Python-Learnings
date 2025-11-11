3005522f9580e7985c9715f2caa47724dbd010d1
TK-Pklgs <talhakhalilsqa@paklogics.com>
2025-11-12 00:05:40 +0500

    Completed chapter 1

--- Chapter 1 (Getting Started)/learnigs.py
+++ Chapter 1 (Getting Started)/learnigs.py
@@ -1,1 +1,7 @@
-print("Hello World")
+# Getting Started to learn python
+"""Getting Started to learn python"""
+# import pyjokes
+# joke = pyjokes.get_joke()
+# print("Hello World")
+
+# print(joke)
