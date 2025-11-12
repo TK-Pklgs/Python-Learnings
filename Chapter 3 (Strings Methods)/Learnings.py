15062811f5189d05e5aada67ca219fee8dbe14e2
TK-Pklgs <talhakhalilsqa@paklogics.com>
2025-11-12 21:48:30 +0500

    Learning Chapter Chapter 3

---
+++ Chapter 3 (Strings Methods)/Learnings.py
@@ -0,0 +1,16 @@
+"""String methods"""
+demo = "Software \n \t Quality \n \t \tQssuarance"
+# print(len(demo))
+# first_demo = demo[0:7:2]
+# # print(demo[0:4])
+# print(first_demo)
+
+# print(demo.endswith(""))
+
+# print(demo.capitalize())
+
+# print(demo.find("Ass"))
+
+# print(demo.title())
+
+print(demo)
---
+++ Chapter 3 (Strings Methods)/Prectice Set (Chapter 3).py
@@ -0,0 +1,28 @@
+"""************************************************************"""
+
+"""Question # 1"""
+"""Write a python program to display a user entered name followed by Good
+Afternoon using input () function."""
+# name = input("Enter Your Name: ")
+# print("Good Afternoon!", name)
+
+"""************************************************************"""
+"""Question # 2"""
+"""Write a program to fill in a letter template given below with name and date.
+letter = """'''
+Dear <|Name|>,
+You are selected!
+<|Date|>
+'''
+name = input("Enter Your Name: ")
+date = input("Enter Date: ")
+
+print(f"Dear {name},\nYour Are Selected!\n{date}")
+
+"""************************************************************"""
+"""************************************************************"""
+"""************************************************************"""
+"""************************************************************"""
+"""************************************************************"""
+"""************************************************************"""
+"""************************************************************"""
