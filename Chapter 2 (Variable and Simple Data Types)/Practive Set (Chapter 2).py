b52369f0a3f85971cf91582746071ecf8d13c26b
TK-Pklgs <talhakhalilsqa@paklogics.com>
2025-11-12 20:54:28 +0500

    Comppleted Chapter 2

---
+++ Chapter 2 (Variable and Simple Data Types)/Practive Set (Chapter 2).py
@@ -0,0 +1,50 @@
+"""Write a python program to add two numbers."""
+
+# first_num = int(input("Enter First Number:"))
+# second_num = int(input("Enter Second Number:"))
+
+# sum = first_num + second_num
+
+# print("Total is:",sum)
+
+"""******************************************************************************"""
+
+"""Write a python program to find remainder when a number is divided by z."""
+
+# print(7%2)
+
+"""Check the type of variable assigned using input () function."""
+# It's a string data type
+
+"""*******************************************************************************"""
+
+"""Use comparison operator to find out whether ‘a’ given variable a is greater than
+‘b’ or not. Take a = 34 and b = 80"""
+
+# a = 34
+# b = 80
+
+# print (a>b)
+
+# Its return false as the a is not bigger than b
+
+"""********************************************************************************"""
+
+"""Write a python program to find an average of two numbers entered by the user."""
+
+# first_num = int(input("Enter your First Number: "))
+# Second_num = int(input("Enter your Second Number: "))
+
+# avg = (first_num + Second_num) / 2
+
+# print("Average is :",avg)
+
+"""********************************************************************************"""
+
+"""Write a python program to calculate the square of a number entered by the user"""
+
+# num = int(input("Enter your Number: "))
+
+# squared = num * num
+
+# print("You Number Sqaured is:",squared)
--- Chapter 2 (Variable and Simple Data Types)/learnings.py
+++ Chapter 2 (Variable and Simple Data Types)/learnings.py
@@ -1,1 +1,24 @@
-print("hello world")
+"""# # Basic Data Types:"""
+# integer = 123
+# floating_point_numbers = 12.34
+# strings = "Hello World"
+# Bolean = True
+# Null_Datatype = None
+
+"""# # Operators in Python"""
+# # 1. Arithmetic operators: +, -, *, / etc.
+# # 2. Assignment operators: =, +=, -= etc.
+# # 3. Comparison operators: ==, >, >=, <, != etc.
+# # 4. Logical operators: and, or, not.
+
+"""# # Types() function and castin"""
+
+# a = float(integer)
+# # print(type(integer))
+# print(type(a)"")
+
+"""Input function"""
+# a = int(input("Enter First Number:"))
+# b = int(input("Enter Second Number:"))
+
+# print ("Your Total Sum is:",a + b)
