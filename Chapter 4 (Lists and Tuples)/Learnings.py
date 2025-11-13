98300b20309ad0766e22ba4063b606859c02c68c
TK-Pklgs <talhakhalilsqa@paklogics.com>
2025-11-13 23:05:54 +0500

    Chapter 4 is Completed

---
+++ Chapter 4 (Lists and Tuples)/Learnings.py
@@ -0,0 +1,29 @@
+"""Lists and its methods Learning"""
+
+my_list = ["Apple", "Banana" , 124 , 123.45 , False]
+numbers = [12,1,2,5,3,4,20,100,99,66,22,11,999]
+# del numbers[0]
+# new_list = numbers + my_list
+print(sorted(numbers))
+
+# A list can contain any type of data types, also unlike strings list are mutable which means it can be changed later
+
+# print(my_list)
+
+# my_list.append(True)
+# print(my_list)
+
+# numbers.sort()
+# numbers.reverse()
+# numbers.append("Kiwi")
+# numbers.insert(1,33)
+# numbers.pop(2)
+# numbers.remove(999)
+# print(numbers.pop(3))
+
+# print(numbers.index(2))
+
+"""Tuple and its methods learning"""
+# numbs = (1,)
+# print(type(numbs))
+# print(numbs)
---
+++ Chapter 4 (Lists and Tuples)/Working_With_Lists.py
@@ -0,0 +1,35 @@
+"""Looping Through a List"""
+# numbers_list = [945, 976, 114, 20, 343, 688, 601, 193, 92, 104, 512, 231, 77, 845, 667, 430, 28, 999, 402, 215]
+strings_list = [ 'alice', 'charlie', 'frank', 'ivan', 'bob', 'Hannah', 'David', 'Grace', 'Eva', 'Julia']
+
+# for names in strings_list:
+#     print(f"{names.title()}, You are a great magician!")
+#     print(f"I can't wait to see your next trick, {names.title()}\n")
+
+# print("Thank you, every one. That was a great magic show!")
+
+"""Range Function()"""
+# Remeber always use one more than the actual range you want to run the code becuase it is always off by one it is a behaviour of the range function.
+# for value in range(1,11):
+#     print(value)
+# for value in range(11):
+#     print(value)
+# squared = []
+# for value in range(1,11):
+#     squared.append(value ** 2)
+# print(squared)
+
+"""Functions that can be perform numbers of list"""
+
+# print(sum(numbers_list))
+# print(min(numbers_list))
+# print(max(numbers_list))
+
+"""List comprehensions"""
+"""In Python, list comprehension is a concise way to create a list using a single line of code. It combines a for loop and optional conditions inside square brackets."""
+
+# squares = [value ** 2 for value in range(1,11)]
+# print(squares)
+
+# for values in strings_list[:3]:
+#     print(values)
